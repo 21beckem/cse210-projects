a6fe58855633bb119882c23919465caa474e7b45
Michael Becker <21beckem8@gmail.com>
2025-09-23 14:34:32 -0600

    completed csharp-prep Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int perc = int.Parse(Console.ReadLine());
+        int lastDig = perc % 10;
+
+        string sign = "";
+        if (lastDig >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDig < 3)
+        {
+            sign = "-";
+        }
+
+        string letterGrade;
+        if (perc >= 90)
+        {
+            if (perc >= 97)
+            {
+                sign = "";
+            }
+            letterGrade = "A";
+        }
+        else if (perc >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (perc >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (perc >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter-grade is: {letterGrade}{sign}");
     }
 }
