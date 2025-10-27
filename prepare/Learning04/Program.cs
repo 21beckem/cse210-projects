04324f07386d538153f12fcaf1ef7290914560e6
Michael Becker <21beckem8@gmail.com>
2025-10-27 10:07:43 -0600

    Completed Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new("Michael", "Programming");
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine();
+
+        MathAssignment math = new("Matthew", "Fractions", "7.2", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment write = new("Joe", "Creative Writing", "Personal Narrative");
+        Console.WriteLine(write.GetSummary());
+        Console.WriteLine(write.GetWritingInformation());
     }
 }
