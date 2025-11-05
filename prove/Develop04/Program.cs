e54fadb56c0323395427ebb723d570db7786a5d2
Michael Becker <21beckem8@gmail.com>
2025-11-05 09:57:26 -0700

    Made main menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,7 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        ListingActivity act = new();
-        act.Start();
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu Options");
+            Console.WriteLine("  1. Start Breathing Activity");
+            Console.WriteLine("  2. Start Reflecting Activity");
+            Console.WriteLine("  3. Start Listing Activity");
+            Console.WriteLine("  4. Chat with a specialist");
+            Console.WriteLine("  5. Quit");
+            Console.Write("Select a choice from the menu: ");
+            string key = Console.ReadLine();
+            if (key == "1")
+            {
+                BreathingActivity act = new();
+                act.Start();
+            }
+            else if (key == "2")
+            {
+                ReflectionActivity act = new();
+                act.Start();
+            }
+            else if (key == "3")
+            {
+                ListingActivity act = new();
+                act.Start();
+            }
+            else if (key == "4")
+            {
+                // chat with specialist
+            }
+            else if (key == "5")
+            {
+                break;
+            }
+        }
     }
 }
